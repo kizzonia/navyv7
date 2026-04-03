b832a519a7779647b80c5044f32dd178e74dca55
kizzonia <chibuikefortune@hotmail.com>
2026-04-03 04:16:17 +0100

    resend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,19 +56,29 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
-  config.action_mailer.default_url_options = { host: 'https://www.navyscreditunion.com' }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+#   config.action_mailer.default_url_options = { host: 'https://www.navyscreditunion.com' }
+#   config.action_mailer.perform_deliveries = true
+#   config.action_mailer.raise_delivery_errors = true
   
-  config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.delivery_method = :smtp
+# config.action_mailer.smtp_settings = {
+#   address:   'smtp.resend.com',
+#   port:      465,
+#   user_name: 'resend',
+#   password:  ENV['RESEND_API_KEY'],
+#   tls:       true,
+#   # open_timeout: 30,    # ← add this
+#   # read_timeout: 30     # ← add this
+# }
+config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
-  address:   'smtp.resend.com',
-  port:      465,
-  user_name: 'resend',
-  password:  ENV['RESEND_API_KEY'],
-  tls:       true,
-  # open_timeout: 30,    # ← add this
-  # read_timeout: 30     # ← add this
+  address:              "smtp.resend.com",
+  port:                 587,
+  user_name:            "resend",
+  password:             ENV["RESEND_API_KEY"],
+  authentication:       :plain,
+  enable_starttls_auto: true
 }
-
+config.action_mailer.default_url_options = { host: "navyscreditunion.com" }
+config.action_mailer.raise_delivery_errors = true
 end
